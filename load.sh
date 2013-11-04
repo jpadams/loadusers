c400d5b3856ad3c61b37e7f89514cf954d33467a
jpadams <jeremy@puppetlabs.com>
2013-11-03 23:49:21 -0800

    Update load.sh

--- load.sh
+++ load.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 
+# may have to remove /opt/puppet/share/puppet/modules/pe_puppetdb/lib/facter/puppetdb_server_status.rb
 for name in `cat /tmp/loadusers/names`; do /tmp/loadusers/mkuser.sh $name; done
